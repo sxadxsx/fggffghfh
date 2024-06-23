0180b5fd3c5d9ed90ad2e47f23f6d38b57f0c0ea
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-06-23 18:21:55 +0800

    Update index.tsx

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,12 +1,12 @@
-import { Suspense } from "react";
+import { Suspense } from "react"
 
-import { listRegions } from "@lib/data";
-import LocalizedClientLink from "@modules/common/components/localized-client-link";
-import CartButton from "@modules/layout/components/cart-button";
-import SideMenu from "@modules/layout/components/side-menu";
+import { listRegions } from "@lib/data"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import CartButton from "@modules/layout/components/cart-button"
+import SideMenu from "@modules/layout/components/side-menu"
 
 export default async function Nav() {
-  const regions = await listRegions();
+  const regions = await listRegions().then((regions) => regions)
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
@@ -25,6 +25,7 @@ export default async function Nav() {
               data-testid="nav-store-link"
             >
               <img className="h-8 w-auto" src="/logo.png" alt="" />
+        
             </LocalizedClientLink>
           </div>
 
@@ -65,5 +66,5 @@ export default async function Nav() {
         </nav>
       </header>
     </div>
-  );
+  )
 }
