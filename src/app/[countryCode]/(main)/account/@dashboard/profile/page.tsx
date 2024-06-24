b5028744489bcccd1c7146a18fc96b82b53c8ec7
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-06-24 13:24:57 +0800

    Update page.tsx

--- src/app/[countryCode]/(main)/account/@dashboard/profile/page.tsx
+++ src/app/[countryCode]/(main)/account/@dashboard/profile/page.tsx
@@ -10,8 +10,8 @@ import { getCustomer, listRegions } from "@lib/data"
 import { notFound } from "next/navigation"
 
 export const metadata: Metadata = {
-  title: "Profile",
-  description: "View and edit your Medusa Store profile.",
+  title: "簡介",
+  description: "查看和修改簡介.",
 }
 
 export default async function Profile() {
@@ -25,11 +25,11 @@ export default async function Profile() {
   return (
     <div className="w-full" data-testid="profile-page-wrapper">
       <div className="mb-8 flex flex-col gap-y-4">
-        <h1 className="text-2xl-semi">Profile</h1>
+        <h1 className="text-2xl-semi">簡介</h1>
         <p className="text-base-regular">
-          View and update your profile information, including your name, email,
-          and phone number. You can also update your billing address, or change
-          your password.
+          檢視和更新您的個人資料資訊，包括您的姓名、電子郵件、
+和電話號碼。 您也可以更新您的賬單地址，或更改
+你的密碼。
         </p>
       </div>
       <div className="flex flex-col gap-y-8 w-full">
